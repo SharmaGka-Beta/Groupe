fc7b9566eabbb41c7b61a6b255644539c8afa028
naman <namansharma2026@gmail.com>
2026-03-18 18:15:58 +0530

    add functions.py and database.py

---
+++ functions.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix = "sin ", intents = intents)
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import discord
-from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import functions
 
 
 load_dotenv()
 token = os.getenv("TOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
 
-bot = commands.Bot(command_prefix = "sin ", intents = intents)
-
-@bot.event
-async def on_ready():
-    print("Bot is ready")
-
-bot.run(token)
+functions.bot.run(token)
 
