cf73ee982bef3d2dc43134ad2f07a5ea08a21b66
naman <namansharma2026@gmail.com>
2026-03-23 19:11:54 +0530

    add police catch event

---
+++ events.py
@@ -0,0 +1,19 @@
+import random
+import database
+import messages
+
+async def police_catch(ctx):
+    info = database.get_user(ctx.author.id)
+    catch_roll = 15 + info['wanted'] * 0.8
+    if (catch_roll <= random.randint(1, 100)):
+        return 0
+    
+    await ctx.send("You have been captured by the police!")
+    await ctx.send("You can bribe, run or talk it out")
+
+    database.update_role(ctx.author.id, 'convict')
+
+    return 1
+    
+    
+
